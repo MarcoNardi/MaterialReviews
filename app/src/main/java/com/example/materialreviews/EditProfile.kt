f65a3ecb6ce6c6d928fe17492ae8d8346e611648
MarcoNardi <marconardi1990@gmail.com>
2022-06-01 20:02:21 +0200

    ora sono 99% sicuro che ottenere l'immagine funzioni

--- app/src/main/java/com/example/materialreviews/EditProfile.kt
+++ app/src/main/java/com/example/materialreviews/EditProfile.kt
@@ -8,6 +8,7 @@ import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -65,7 +66,6 @@ fun EditProfile(model: UserViewModel) {
     val name = (user?.firstName ?: "help")
     val surname = (user?.lastName ?: "halp")
     val profile_image = user?.imageUri
-
     if(profile_image != "") {
         val t = Toast.makeText(context, "NOPE", Toast.LENGTH_LONG)
         t.show()
@@ -86,8 +86,12 @@ fun EditProfile(model: UserViewModel) {
         var bitmap by remember { mutableStateOf<Bitmap?>(null) }
         val launcher = rememberLauncherForActivityResult(contract =
         ActivityResultContracts.GetContent()) { uri: Uri? ->
-            imageUri = uri
-            model.updateImageOfUser(login_id, imageUri.toString())
+
+            if(uri.toString()!="null"){
+                imageUri = uri
+                model.updateImageOfUser(login_id, imageUri.toString())
+
+            }
         }
 
 
@@ -97,7 +101,8 @@ fun EditProfile(model: UserViewModel) {
             if(profile_image==null){
                 ProfilePicture(size = 150.dp)
             }else{
-                if(profile_image=="")ProfilePicture(size = 150.dp)else{
+                Log.v("aibsua", profile_image)
+                if(profile_image=="" || profile_image=="null")ProfilePicture(size = 150.dp)else{
                    val imageBitmat = getImageBitmap(profile_image, context)
                    Image(
                        bitmap = imageBitmat!!.asImageBitmap(),
