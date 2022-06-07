4e72be38812cadc7b62ac52188dfc1fa1f9e80bc
Eli Ferin <eli.ferin@studenti.unipd.it>
2022-06-07 13:14:46 +0200

    Fixato warning

--- app/src/main/java/com/example/materialreviews/MainActivity.kt
+++ app/src/main/java/com/example/materialreviews/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,6 +39,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalAnimationApi
+@ExperimentalComposeUiApi
 @ExperimentalMaterial3Api
 @Preview
 @Composable
