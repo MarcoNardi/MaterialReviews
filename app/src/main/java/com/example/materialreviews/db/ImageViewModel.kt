dbdaf4b3b0ab3ce673923d6c45893ad150c5520b
MarcoNardi <marconardi1990@gmail.com>
2022-06-13 17:40:02 +0200

    trasformati i commenti dei viewmodel in javadocs

--- app/src/main/java/com/example/materialreviews/db/ImageViewModel.kt
+++ app/src/main/java/com/example/materialreviews/db/ImageViewModel.kt
@@ -5,20 +5,30 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-
-//viewModel dell'imageEntity che si occupa di interfacciarsi al db
+/**
+ *
+viewModel dell'imageEntity che si occupa di interfacciarsi al db
+ */
 class ImageViewModel(private val imageDao: ImageDao) : ViewModel() {
-    //helper per inserire un immagine
+    /**
+     *
+    per inserire un immagine
+      */
     private fun insertImage(image: ImageEntity) {
         viewModelScope.launch{
             imageDao.insert(image)
         }
     }
-    //helper per creare un entry
+
+    /**
+     * helper per creare un entry
+     */
     private fun createImageEntry(uri: String, rid: Int): ImageEntity{
         return ImageEntity(uri, rid )
     }
-    //funzione per inserire un immagine
+    /**
+     *funzione per inserire un immagine
+     */
     fun addImage(uri: String, rid: Int){
         insertImage(createImageEntry(uri, rid))
     }
@@ -26,6 +36,7 @@ class ImageViewModel(private val imageDao: ImageDao) : ViewModel() {
 
 
 }
+
 //Pattern factory per creare un viewModel codice identico per ogni viewModel
 class ImageViewModelFactory(private val imageDao: ImageDao): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
--- app/src/main/java/com/example/materialreviews/db/ReviewViewModel.kt
+++ app/src/main/java/com/example/materialreviews/db/ReviewViewModel.kt
@@ -8,38 +8,52 @@ import kotlinx.coroutines.launch
 //viewModel che si interfaccia alle reviews
 class ReviewViewModel(private val reviewDao: ReviewDao) : ViewModel() {
 
-    //helper per inserire reviews
+    /**
+     *    helper per inserire reviews
+     */
     private fun insertReview(review: ReviewEntity) {
         viewModelScope.launch{
             reviewDao.insert(review)
         }
     }
-    //helper pre creare un'entry
+    /**
+     *     helper pre creare un'entry
+     */
     private fun createReviewEntry(rating: Int, review: String, userId: Int, restaurantId: Int, date:String): ReviewEntity{
         return ReviewEntity(rating,review, userId, restaurantId,date)
     }
-    //add a review
+    /**
+     * aggiunge una review
+     */
     fun addReview(rating: Int, review: String,userId: Int, restaurantId: Int, date:String){
         insertReview(createReviewEntry(rating,review, userId, restaurantId, date))
     }
-    //add a review
+    /**
+     * aggiunge una review
+     */
     fun addReview(review: ReviewEntity){
         insertReview(review)
     }
 
-    //aggiorna una review
+    /**
+     *aggiorna una review
+     */
     fun updateReview(restaurantId: Int, userId: Int, newComment:String, newDate:String, newRating: Int){
         viewModelScope.launch {
             reviewDao.updateReview(restaurantId, userId, newComment, newDate, newRating)
         }
     }
-    //cancella una review
+    /**
+     *cancella una review
+     */
     fun deleteReview(review: ReviewEntity){
         viewModelScope.launch {
             reviewDao.delete(review)
         }
     }
-    //cancella una review in base alla sua primary key
+    /**
+     *cancella una review in base alla sua primary key
+     */
     fun deleteReviewByIds(restaurantId: Int, userId: Int){
         viewModelScope.launch {
             reviewDao.deleteByIds(restaurantId, userId)
